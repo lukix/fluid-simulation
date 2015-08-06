05be489f5d75982a5b1e2b25ed851c0823bea779
lukix <lukix.95@gmail.com>
2015-08-06 13:12:02 +0200

    Change type of rain drops from Particle to WaterParticle

--- js/worldConfig/ui/rainModule.js
+++ js/worldConfig/ui/rainModule.js
@@ -1,11 +1,11 @@
-define(['../../fluid-simulation-engine/base/Particle'], function (Particle) {
+define(['../../fluid-simulation-engine/base/WaterParticle'], function (WaterParticle) {
 	return function (world) {
 		var rainClock = null;// = setInterval(rainGenerator, 100);
 		function rainGenerator() {
 			var drops = [];
 			const MARGIN = 10;
 			for(var i = 0; i < 1; i++) {
-				drops.push(new Particle(MARGIN+Math.random()*(world.width-2*MARGIN), -Math.random()*50));
+				drops.push(new WaterParticle(MARGIN+Math.random()*(world.width-2*MARGIN), -Math.random()*50));
 			}
 			world.addParticles(drops);
 		};
