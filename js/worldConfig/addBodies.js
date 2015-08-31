470ebc538c6008e192b14e23acd7851e7e9c0b03
lukix <lukix.95@gmail.com>
2015-09-01 00:39:51 +0200

    Make addBodies.js code cleaner

--- js/worldConfig/addBodies.js
+++ js/worldConfig/addBodies.js
@@ -1,53 +1,44 @@
-define(['../fluid-simulation-engine/base/Body', '../fluid-simulation-engine/geometry/Vector'], function (Body, Vector) {
+define(
+	[
+		 '../fluid-simulation-engine/base/Body'
+		,'../fluid-simulation-engine/geometry/Vector'
+	], function (Body, Vector) {
 	return function (world) {
-		var width = 1200;
-		var height = 500;
-		//Bottom wall
-		world.addBody(new Body(
-			 new Vector(-(100-10), 0)
-			,new Vector(width+(100-10), 0)
-			,new Vector(width+(100-10), 100)
-			,new Vector(-(100-10), 100)
-		).setCoords(new Vector(0, height-10)));
+		var width = 1300;
+		var height = 1000;
+		var wallThickness = 100;
 
-		//Left wall
-		world.addBody(new Body(
-			 new Vector(0, 0)
-			,new Vector(-100, 0)
-			,new Vector(-100, 2*height+(100-10))
-			,new Vector(0, 2*height+(100-10))
-		).setCoords(new Vector(10, -height)));
+		var horizontalWall = [
+			new Vector(0, 0)
+		 ,new Vector(width, 0)
+		 ,new Vector(width, wallThickness)
+		 ,new Vector(0, wallThickness)
+	 	];
+		var verticalWall = [
+			new Vector(0, 0)
+		 ,new Vector(wallThickness, 0)
+		 ,new Vector(wallThickness, height+wallThickness)
+		 ,new Vector(0, height+wallThickness)
+		];
+		var shortHorizontalWall = [
+			new Vector(0, 0)
+		 ,new Vector(0, 50)
+		 ,new Vector(550, 50)
+		 ,new Vector(550, 0)
+	 	];
+		var shortVerticalWall = [
+			new Vector(0, 0)
+		 ,new Vector(0, 100)
+		 ,new Vector(50, 100)
+		 ,new Vector(50, 0)
+	 	];
 
-		//Right wall
-		world.addBody(new Body(
-			 new Vector(0, 0)
-			,new Vector(100, 0)
-			,new Vector(100, 2*height+(100-10))
-			,new Vector(0, 2*height+(100-10))
-		).setCoords(new Vector(width-10, -height)));
+		world.addBody(new Body(horizontalWall).setCoords(new Vector(0, height)));	//Bottom wall
+		world.addBody(new Body(verticalWall).setCoords(new Vector(0, 0)));	//Left wall
+		world.addBody(new Body(verticalWall).setCoords(new Vector(width-wallThickness, 0)));	//Right wall
+		world.addBody(new Body(horizontalWall).setCoords(new Vector(0, 0)));	//Top wall
 
-		//Top wall
-		world.addBody(new Body(
-			 new Vector(-(100-10), 0)
-			,new Vector(width+(100-10), 0)
-			,new Vector(width+(100-10), 100)
-			,new Vector(-(100-10), 100)
-		).setCoords(new Vector(0, -height)));
-
-		//----------------------------------
-
-		world.addBody(new Body(
-			 new Vector(0, 0)
-			,new Vector(0, 100)
-			,new Vector(50, 100)
-			,new Vector(50, 0)
-		).setCoords(new Vector(450, 300)));
-
-		world.addBody(new Body(
-			 new Vector(0, 0)
-			,new Vector(0, 50)
-			,new Vector(500, 50)
-			,new Vector(500, 0)
-		).setCoords(new Vector(-40, 350)));
+		world.addBody(new Body(shortVerticalWall).setCoords(new Vector(500, 800)));
+		world.addBody(new Body(shortHorizontalWall).setCoords(new Vector(0, 850)));
 	};
 });
--- js/worldConfig/initWorld.js
+++ js/worldConfig/initWorld.js
@@ -10,12 +10,12 @@ define(
 	return function () {
     var world = new World();
     addBodies(world);
-    world.addParticlesGrid(25, 40, 50, -240, WaterParticle);  //rows, cols, x, y, type
+    world.addParticlesGrid(25, 40, 130, 130, WaterParticle);  //rows, cols, x, y, type
 
     (function (world) {
       var minPoint = world.getBodiesMinPoint();
       var maxPoint = world.getBodiesMaxPoint();
-      world.setOutOfBoundsBehaviour(new Vector(600, -300), function (particle) {
+      world.setOutOfBoundsBehaviour(new Vector(650, 200), function (particle) {
         if(particle.coords.y > maxPoint.y || particle.coords.y < minPoint.y || particle.coords.x > maxPoint.x || particle.coords.x < minPoint.x)
           return true;
         else
