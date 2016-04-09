f157f6515e667a40f6717d68533933e466d0528b
lukix <lukix.95@gmail.com>
2016-04-09 17:59:19 +0200

    Remove undefined cells from Grid

--- js/fluid-simulation-engine/base/Grid.js
+++ js/fluid-simulation-engine/base/Grid.js
@@ -3,6 +3,7 @@ define(function () {
 		this.particles = particles;
 		this.cellSize = cellSize;
 		this.gridStart;
+		this.gridEnd;
 		this.gridArray = [];
 	}
 	Grid.prototype.setCellSize = function (cellSize) {
@@ -15,58 +16,65 @@ define(function () {
 			x: this.particles[0].coords.x,
 			y: this.particles[0].coords.y
 		};
+		this.gridEnd = {
+			x: this.particles[0].coords.x,
+			y: this.particles[0].coords.y
+		};
 		for(var i = 0; i < this.particles.length; i++) {
 			if(this.particles[i].coords.x < this.gridStart.x)
 				this.gridStart.x = this.particles[i].coords.x;
-
 			if(this.particles[i].coords.y < this.gridStart.y)
 				this.gridStart.y = this.particles[i].coords.y;
+			if(this.particles[i].coords.x > this.gridEnd.x)
+				this.gridEnd.x = this.particles[i].coords.x;
+			if(this.particles[i].coords.y > this.gridEnd.y)
+				this.gridEnd.y = this.particles[i].coords.y;
 		}
 	}
 	Grid.prototype.update = function () {
 		this.init();
-		this.gridArray = [];
+		var width = Math.ceil((this.gridEnd.x-this.gridStart.x)/this.cellSize);
+		var height = Math.ceil((this.gridEnd.y-this.gridStart.y)/this.cellSize)
+		this.gridArray = new Array(width);
+		for(var i = 0; i < width; i++) {
+			this.gridArray[i] = new Array(height);
+			for(var j = 0; j < height; j++) {
+				this.gridArray[i][j] = [];
+			}
+		}
 
 		for(var i = 0; i < this.particles.length; i++) {
 			var x = Math.floor((this.particles[i].coords.x - this.gridStart.x) / this.cellSize);
 			var y = Math.floor((this.particles[i].coords.y - this.gridStart.y) / this.cellSize);
-			if(this.gridArray[x] === undefined)
-				this.gridArray[x] = [];
-			if(this.gridArray[x][y] === undefined)
-				this.gridArray[x][y] = [];
 			this.gridArray[x][y].push(this.particles[i]);
 		}
 	}
-	Grid.prototype.forEachPair = function (callback, context) {
-		//Preparing checkArray
+	Grid.prototype.createCheckedArray = function() {
 		var checkedArray = [];
 		for(var X = 0; X < this.gridArray.length; X++) {
-			if(this.gridArray[X] === undefined)
-				continue;
 			checkedArray[X] = [];
 			for(var Y = 0; Y < this.gridArray[X].length; Y++) {
-				if(this.gridArray[X][Y] === undefined)
-					continue;
 				checkedArray[X][Y] = [];
 				for(var i = this.gridArray[X][Y].length - 1; i >= 0; i--) {
 					checkedArray[X][Y][i] = false;
 				}
 			}
 		}
+		return checkedArray;
+	}
+	Grid.prototype.forEachPair = function (callback, context) {
+		//Preparing checkArray
+		var checkedArray = this.createCheckedArray();
 
 		//Main loop
 		for(var X = 0; X < this.gridArray.length; X++) {
-			if(this.gridArray[X] === undefined)
-				continue;
 			for(var Y = 0; Y < this.gridArray[X].length; Y++) {
-				if(this.gridArray[X][Y] === undefined)
-					continue;
 				for(var i = this.gridArray[X][Y].length - 1; i >= 0; i--) {
 					for(var x = X-1; x <= X+1; x++) {
-						if(this.gridArray[x] === undefined)
+						if(x < 0 || x >= this.gridArray.length)
 							continue;
 						for(var y = Y-1; y <= Y+1; y++) {
-							if(this.gridArray[x][y] === undefined)
+							if(y < 0 || y >= this.gridArray[x].length)
 								continue;
 							for(var j = 0; j < this.gridArray[x][y].length; j++) {
 								if(checkedArray[x][y][j] || (X === x && Y === y && i === j))
